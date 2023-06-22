b1b1dd099955a1724241a4461fbcc623cce635ab
berylcl <gakiiberyl@gmail.com>
2023-06-22 11:00:30 +0100

    Fixing eslint error

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,32 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Profile = () => {
-  const dragons = useSelector((state) => state.dragons);
-  const reservedDragons = dragons.filter((dragon) => dragon.reserved);
-
-  return (
-    <div>
-      <h2>My Profile</h2>
-      <div className="dragons-table">
-        <table>
-          <thead>
-            <tr>
-              <th>My Dragons</th>
-            </tr>
-          </thead>
-          <tbody>
-            {reservedDragons.map((dragon) => (
-              <tr key={dragon.id}>
-                <td>{dragon.id}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-import { useSelector } from 'react-redux';
-
-const Profile = () => {
   const missions = useSelector((state) => state.mission.missions);
   const reservedMissions = missions.filter((mission) => mission.reserved);
+  const dragons = useSelector((state) => state.dragons);
+  const reservedDragons = dragons.filter((dragon) => dragon.reserved);
   return (
     <div className="profileBox" data-id="profile-box">
       <div className="missionsProfile">
@@ -42,6 +20,25 @@ const Profile = () => {
               {reservedMission.mission_name}
             </div>
           ))}
+        </div>
+      </div>
+      <div>
+        <h2>My Profile</h2>
+        <div className="dragons-table">
+          <table>
+            <thead>
+              <tr>
+                <th>My Dragons</th>
+              </tr>
+            </thead>
+            <tbody>
+              {reservedDragons.map((dragon) => (
+                <tr key={dragon.id}>
+                  <td>{dragon.id}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
