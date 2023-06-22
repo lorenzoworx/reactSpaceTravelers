cc31dbe5beb6ac453b256168e03421a1e0282e2d
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-06-22 09:43:29 +0100

    ADD profile rockets section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,47 @@
-const Profile = () => (
-  <div>My Profile</div>
-);
+import React from 'react';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
+function ReservedRocketsTable({ rockets }) {
+  return (
+    <table>
+      <tbody>
+        {rockets.map((rocket) => (
+          <tr key={rocket.id}>
+            <td>{rocket.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+ReservedRocketsTable.propTypes = {
+  rockets: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+const Profile = () => {
+  const { rockets } = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter(
+    (rocket) => rocket.reserved === true,
+  );
+
+  return (
+    <div>
+      <div className="rocketsProfile">
+        <h2>My Rockets</h2>
+        {reservedRockets.length > 0 ? (
+          <ReservedRocketsTable rockets={reservedRockets} />
+        ) : (
+          <div>No Rockets Reserved</div>
+        )}
+      </div>
+    </div>
+  );
+};
 export default Profile;
